41e8e79c8c9269c1a4900aa7e71a04ffa0517fc4
Gianfranco Ilarraz <gianfrancoilarraz12@gmail.com>
2023-06-01 20:41:52 -0300

    Generador de productos en forma de cards en la page, usando mapping y promises

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-key */
 // import './ItemListContainer.css'
 import { useState } from "react"
 import { useEffect } from "react"
@@ -6,27 +7,59 @@ import { useEffect } from "react"
 const MOCK_DATA = [
     {
         ID: 1,
+        Nombre: 'Producto1',
         Precio: 1500,
         Descripción: 'Lorem ipsum dolor sit',
-        img: "http://dummyimages.com/"
+        img: "https://dummyimage.com/300x300/000/e3e3e3.png"
     },
     {
         ID: 2,
-        Precio: 1500,
-        Descripción: 'Lorem ipsum dolor sit',
-        img: "http://dummyimages.com/"
+        Nombre: 'Producto2',
+        Precio: 1200,
+        Descripción: 'Lorem ipsum dolor sleep',
+        img: "https://dummyimage.com/300x300/000/e3e3e3.png"
     },
     {
         ID: 3,
-        Precio: 1500,
-        Descripción: 'Lorem ipsum dolor sit',
-        img: "http://dummyimages.com/"
+        Nombre: 'Producto3',
+        Precio: 500,
+        Descripción: 'Lorem ipsum dolor stand',
+        img: "https://dummyimage.com/300x300/000/e3e3e3.png"
     },
     {
         ID: 4,
-        Precio: 1500,
-        Descripción: 'Lorem ipsum dolor sit',
-        img: "http://dummyimages.com/"
+        Nombre: 'Producto4',
+        Precio: 1700,
+        Descripción: 'Lorem ipsum dolor jump',
+        img: "https://dummyimage.com/300x300/000/e3e3e3.png"
+    },
+    {
+        ID: 5,
+        Nombre: 'Producto5',
+        Precio: 1700,
+        Descripción: 'Lorem ipsum dolor jump',
+        img: "https://dummyimage.com/300x300/000/e3e3e3.png"
+    },
+    {
+        ID: 6,
+        Nombre: 'Producto6',
+        Precio: 1700,
+        Descripción: 'Lorem ipsum dolor jump',
+        img: "https://dummyimage.com/300x300/000/e3e3e3.png"
+    },
+    {
+        ID: 7,
+        Nombre: 'Producto7',
+        Precio: 1700,
+        Descripción: 'Lorem ipsum dolor jump',
+        img: "https://dummyimage.com/300x300/000/e3e3e3.png"
+    },
+    {
+        ID: 8,
+        Nombre: 'Producto8',
+        Precio: 1700,
+        Descripción: 'Lorem ipsum dolor jump',
+        img: "https://dummyimage.com/300x300/000/e3e3e3.png"
     },
 
 ]
@@ -59,12 +92,35 @@ const ItemListConainer = () => {
             })
     }, [])
 
+    // const productosMap = MOCK_DATA.map((producto) => {
+    //     <div>
+    //         <h2>{producto.ID}</h2>;
+    //         <h3>{producto.Precio}</h3>;
+    //         <p>{producto.Descripción}</p>;
+    //         <img src={producto.img} alt={producto.ID} />
+    //     </div>
+    // }
+    // )
+
     return (
         <div className="list__container">
             <h2>Bienvenidos a ZtSells</h2>
             <hr />
 
             <p>{productos.length === 0 ? 'No hay productos' : 'Productos cargados'}</p>
+
+            <div className="row">
+                {productos.map((producto) => (
+                    <div key={producto.ID}>
+                        <h2>{producto.ID} {producto.Nombre}</h2>
+                        <h3>{producto.Precio}</h3>
+                        <p>{producto.Descripción}</p>
+                        <img src={producto.img} alt={producto.ID} />
+                    </div>
+                )
+                )}
+            </div>
+
 
 
         </div>
