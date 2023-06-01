887adcaffd0134202c76c4d4e5643adbdec9f2db
Gianfranco Ilarraz <gianfrancoilarraz12@gmail.com>
2023-06-01 20:07:36 -0300

    Carga de datos desde un mock usando promise y hooks

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,44 +1,72 @@
 // import './ItemListContainer.css'
-
+import { useState } from "react"
+import { useEffect } from "react"
 
 //MOCKDATABASE
-const MOCK_DATA ={
-    
-}
+const MOCK_DATA = [
+    {
+        ID: 1,
+        Precio: 1500,
+        Descripción: 'Lorem ipsum dolor sit',
+        img: "http://dummyimages.com/"
+    },
+    {
+        ID: 2,
+        Precio: 1500,
+        Descripción: 'Lorem ipsum dolor sit',
+        img: "http://dummyimages.com/"
+    },
+    {
+        ID: 3,
+        Precio: 1500,
+        Descripción: 'Lorem ipsum dolor sit',
+        img: "http://dummyimages.com/"
+    },
+    {
+        ID: 4,
+        Precio: 1500,
+        Descripción: 'Lorem ipsum dolor sit',
+        img: "http://dummyimages.com/"
+    },
 
+]
 
-//Promises
-const procesoAsync = (bool) => {
-    return new Promise((resolve, reject) => {
-        if (bool) {
-            resolve('Promesa resuelta')
-        } else {
-            reject('Promesa rechazada')
-        }
-    }
-    )
-}
-
-// El res y el err de then y catch, son los valores de resolucion o de rechazo de la promesa
-procesoAsync(false)
-    .then((res) => {
-        console.log(res)
-    })
-    .catch((err) => {
-        console.log(err)
-    })
 
 
 
 // eslint-disable-next-line react/prop-types
-const ItemListConainer = ({ mensaje }) => {
+const ItemListConainer = () => {
+
+    const [productos, setProductos] = useState([])
+    console.log(productos)
+
+    const pedirDatos = () => {
+        return new Promise((resolve) => {
+
+            resolve(MOCK_DATA)
+        })
+    }
+
+
+
+    useEffect(() => {
+        pedirDatos()
+            .then((res) => {
+                setTimeout(() => { setProductos(res) }, 2000)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }, [])
 
     return (
         <div className="list__container">
             <h2>Bienvenidos a ZtSells</h2>
             <hr />
 
-            <p>{mensaje}</p>
+            <p>{productos.length === 0 ? 'No hay productos' : 'Productos cargados'}</p>
+
+
         </div>
     )
 }
